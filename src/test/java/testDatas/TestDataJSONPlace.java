071edad91c25376e61b654d03f3fc5ecf963d8fc
sadik Goden <sadikgoden001@gmail.com>
2023-11-17 02:23:26 +0300

    qwas

--- src/test/java/testDatas/TestDataJSONPlace.java
+++ src/test/java/testDatas/TestDataJSONPlace.java
@@ -1,5 +1,8 @@
 package testDatas;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+
 public class TestDataJSONPlace {
 
     public int okStatusKodu=200;
@@ -44,4 +47,17 @@ public class TestDataJSONPlace {
 
         return reqBody;
     }
+    public HashMap<String,Object> requestBodyOlusturMAP(){
+        HashMap<String,Object> reqBody=new HashMap<>();
+
+
+        reqBody.put("title","Ali");
+        reqBody.put("body","Merhaba");
+        reqBody.put("userId",10.0);
+        reqBody.put("id",70.0);
+
+        return reqBody;
+    }
+
+
 }
